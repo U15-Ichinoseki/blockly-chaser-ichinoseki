8f3767a0b4ebf932b9ce44616f25944c0474057d
U15-Ichinoseki <ichinoseki.procon@gmail.com>
2025-10-16 14:10:58 +0900

    googleタグを挿入

---
+++ views/common/gtag.js
@@ -0,0 +1,9 @@
+<!-- Google tag (gtag.js) -->
+  <script async src="https://www.googletagmanager.com/gtag/js?id=G-LQ2QL2J00M"></script>
+  <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'G-LQ2QL2J00M');
+  </script>
